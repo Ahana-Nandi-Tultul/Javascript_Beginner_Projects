75fd8ebe37dae216bca2af9a5e37718a609c6e24
Ahana Nandi <ahananondi6@gmail.com>
2026-03-25 20:04:50 +0600

    simple calculator

--- Story_website/js/script.js
+++ Story_website/js/script.js
@@ -1,52 +1,50 @@
 const links = document.querySelectorAll(".sidebar a");
-const sections = document.querySelectorAll(".scene");
-const sidebarTitle = document.querySelector(".sidebar-left");
+const sections = document.querySelectorAll('.scene');
+const sidebarTitle = document.querySelector('.sidebar-left');
 
-/* SECTION TITLES */
+// console.log(links, sections, sidebarTitle)
+
 const titles = {
-  scene1: "Beginning",
-  scene2: "Discovery",
-  scene3: "Struggle",
-  scene4: "Learning",
-  scene5: "Growth",
-  scene6: "Success",
-  scene7: "Future"
-};
+    scene1 : "Beginning",
+    scene2 : "Discovery",
+    scene3 : "Struggle",
+    scene4 : "Learning",
+    scene5 : "Growth",
+    scene6 : "Success",
+    scene7 : "Future",
+}
 
-/* ON SCROLL */
 window.addEventListener("scroll", () => {
-  let current = "";
+    let current = "";
+    sections.forEach(section => {
+        const top= section.offsetTop;
+        const height= section.clientHeight;
 
-  sections.forEach(section => {
-    const top = section.offsetTop;
-    const height = section.clientHeight;
+        if(scrollY >= top - height / 3)
+        {
+            current = section.id;
+        }
+        const rect = section.getBoundingClientRect();
+        if(rect.top < window.innerHeight - 100)
+        {
+            section.classList.add("show");
+        }
+    });
 
-    if (scrollY >= top - height / 3) {
-      current = section.id;
+    links.forEach(link => {
+        link.classList.remove('active');
+        if(link.getAttribute("href").includes(current)){
+            link.classList.add('active')
+        }
+    })
+
+    
+    if(titles[current])
+    {
+        sidebarTitle.textContent = titles[current]
     }
+})
 
-    /* reveal animation */
-    const rect = section.getBoundingClientRect();
-    if (rect.top < window.innerHeight - 100) {
-      section.classList.add("show");
-    }
-  });
-
-  /* active link */
-  links.forEach(link => {
-    link.classList.remove("active");
-    if (link.getAttribute("href").includes(current)) {
-      link.classList.add("active");
-    }
-  });
-
-  /* update sidebar title */
-  if (titles[current]) {
-    sidebarTitle.textContent = titles[current];
-  }
-});
-
-/* HERO LOAD ANIMATION */
-window.addEventListener("load", () => {
-  document.querySelector("#scene1").classList.add("show");
-});
+window.addEventListener('load', () => {
+    document.querySelector("#scene1").classList.add("show")
+})
